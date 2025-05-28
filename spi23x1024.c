1ab38771d409d56fd42b5b7204e19b756f0f8431
ZTisha <120229888+ZTisha@users.noreply.github.com>
2025-05-28 14:42:30 -0500

    Update spi23x1024.c

--- spi23x1024.c
+++ spi23x1024.c
@@ -13,7 +13,7 @@ Email: aae0008@auburn.edu
 Modified by: Gaines Odom
 Email: gaines.odom@auburn.edu
 
-Modified by: Zakia Tamanna Tisha
+Further modified by: Zakia Tamanna Tisha
 Email: zakia.tisha@auburn.edu
 
 */
